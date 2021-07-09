45385eff7eda381300d2beca8509b9437e950ab8
Geoffrey Huntley <ghuntley@ghuntley.com>
2021-07-09 07:50:09 +0000

    Tweak all the things

--- digitalocean-droplet/setup.sh
+++ digitalocean-droplet/setup.sh
@@ -5,8 +5,7 @@ set -exuo pipefail
 doctl account get
 
 DROPLET_NAME=gitpod-k3s
-DOMAIN_NAME=ludmann.name
-GITPOD_SUBDOMAIN=gitpod.x
+DOMAIN_NAME=featureflag.com
 LEGO_VOLUME_ID=a705e73d-7d8f-11eb-84cd-0a58ac14d02e
 
 # if given, the helm chart of this commit is used
@@ -19,16 +18,16 @@ DROPLET_SIZE=s-2vcpu-4gb
 
 doctl compute droplet create "$DROPLET_NAME" \
     --image ubuntu-20-04-x64 \
-    --region fra1 \
+    --region sgp1 \
     --size "$DROPLET_SIZE" \
     --ssh-keys "$DIGITALOCEAN_SSH_KEY_FINGERPRINT" \
     --volumes "$LEGO_VOLUME_ID" \
     --wait
 
 IP=$(doctl compute droplet get "$DROPLET_NAME" --format PublicIPv4 --no-header)
-doctl compute domain records create "$DOMAIN_NAME" --record-type A --record-name "$GITPOD_SUBDOMAIN" --record-data "$IP" --record-ttl 30
-doctl compute domain records create "$DOMAIN_NAME" --record-type A --record-name "*.$GITPOD_SUBDOMAIN" --record-data "$IP" --record-ttl 30
-doctl compute domain records create "$DOMAIN_NAME" --record-type A --record-name "*.ws.$GITPOD_SUBDOMAIN" --record-data "$IP" --record-ttl 30
+doctl compute domain records create "$DOMAIN_NAME" --record-type A --record-name "$DOMAIN_NAME" --record-data "$IP" --record-ttl 30
+doctl compute domain records create "$DOMAIN_NAME" --record-type A --record-name "*.$DOMAIN_NAME" --record-data "$IP" --record-ttl 30
+doctl compute domain records create "$DOMAIN_NAME" --record-type A --record-name "*.ws.$DOMAIN_NAME" --record-data "$IP" --record-ttl 30
 
 doctl compute droplet create "${DROPLET_NAME}-wsnode" \
     --image ubuntu-20-04-x64 \
@@ -42,8 +41,8 @@ sleep 30
 
 scp -r gitpod-install "root@$IP:"
 ssh "root@$IP" ./gitpod-install/install.sh \
-    "$GITPOD_SUBDOMAIN.$DOMAIN_NAME" \
-    "letsencrypt@cornelius-ludmann.de" \
+    "$DOMAIN_NAME" \
+    "ghuntley@ghuntley.com" \
     "$DIGITALOCEAN_ACCESS_TOKEN" \
     "$GITPOD_GITHUB_CLIENT_SECRET" \
     "$GITPOD_COMMIT_ID"
